fb2a19a71429f34d3476321b899c73588e6c40ee
123Somrat <jafaruddin875@gmail.com>
2024-04-21 16:38:48 +0200

    handle Single location name route

---
+++ app/api/location/[name]/route.ts
@@ -0,0 +1,15 @@
+import { getLocationByName } from "../location.utils";
+import type { NextApiRequest, NextApiResponse } from "next";
+type location = {
+    name: string;
+    longitude: number;
+    latitude: number;
+  };
+export async function GET(
+  _request: NextApiRequest,
+  { params }: { params: { name: string } }
+,response:NextApiResponse<location>) {
+  const locationName = params?.name;
+  const locationData = getLocationByName(locationName);
+  return Response?.json(locationData);
+}
