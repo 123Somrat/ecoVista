9b3dee8fdbcf838b33f5f5a6f6a748c953298e38
123Somrat <jafaruddin875@gmail.com>
2024-04-22 15:08:00 +0200

    Add styles and import icon and image

--- app/[location]/layout.tsx
+++ app/[location]/layout.tsx
@@ -1,32 +1,43 @@
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../globals.css";
-
+import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function LOcationLayout({
-    children,
-    airQualityIndex,
-    weather,
-    temparature,
-    wind,
-  }: Readonly<{
-    children: React.ReactNode;
-    airQualityIndex : React.ReactNode
-    temparature : React.ReactNode
-    weather : React.ReactNode
-    wind : React.ReactNode
-  }>) {
-    return (
-      <div className="wrapper">
-       {children}
-       {weather}
-       {airQualityIndex}
-       {wind}
-       {temparature}
-      </div>
-     
-    
-    );
-  }
+  children,
+  airQualityIndex,
+  weather,
+  temparature,
+  wind,
+}: Readonly<{
+  children: React.ReactNode;
+  airQualityIndex: React.ReactNode;
+  temparature: React.ReactNode;
+  weather: React.ReactNode;
+  wind: React.ReactNode;
+}>) {
+  return (
+    <div className="wrapper">
+      <div className="overlay"></div>
+      <Image
+        src="/background.png"
+        className="bg-img"
+        width={700}
+        height={1200}
+        alt="backround image"
+      />
+      <main className="!z-50 w-full">
+        <div className="container">
+          <div className="grid grid-cols-12 gap-y-8 py-16 lg:gap-8 2xl:gap-20 2xl:py-20">
+            {children}
+            {weather}
+            {airQualityIndex}
+            {wind}
+            {temparature}
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
