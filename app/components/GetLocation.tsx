93d6cd8aa3aa7de0af82ca0ae0f14e64d1966cd0
123Somrat <jafaruddin875@gmail.com>
2024-04-23 17:14:25 +0200

    Create location Detector component

---
+++ app/components/GetLocation.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+export default function GetLocation() {
+
+
+  return (
+    <div>
+        <h1>Finding your location.....</h1>
+    </div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>eco vista</h1>
-    </main>
-  );
+import GetLocation from "./components/GetLocation";
+
+export default function HomePage() {
+  return <GetLocation />
 }
