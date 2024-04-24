923bd16e2d8ee4e35ec798898f028d65b815a380
123Somrat <jafaruddin875@gmail.com>
2024-04-24 14:19:19 +0200

    Fetching wind data

--- lib/weather-info.ts
+++ lib/weather-info.ts
@@ -1,3 +1,4 @@
+// Fetching weather data
 export const getWeatherData = async (lat: string, lon: string) => {
   try {
     const response = await fetch(
@@ -10,6 +11,8 @@ export const getWeatherData = async (lat: string, lon: string) => {
   }
 };
 
+
+// Fetching temparature data
 export const getTemparatureData = async (lat: string, lon: string) => {
   try {
     const response = await fetch(
@@ -22,5 +25,15 @@ export const getTemparatureData = async (lat: string, lon: string) => {
   }
 };
 
-
-
+// Fetching wind data
+export const getWindData = async (lat: string, lon: string) => {
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.OPEN_WEATHER_MAP_API_KEY}&units=metric`
+    );
+    const windData = await response.json();
+    return windData?.wind;
+  } catch (error) {
+    console.error(error);
+  }
+};
