fe17a1c5e73b7f0b0d557f3909fa35c3d29d938d
123Somrat <jafaruddin875@gmail.com>
2024-04-24 15:10:31 +0200

    CReate card component nd getting synamic params data and serach params data

--- app/[location]/@airQualityIndex/page.tsx
+++ app/[location]/@airQualityIndex/page.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 
-export default function AirQualityIndex() {
-  return <div>AirQuality index</div>;
+export default function AirQualityIndex(
+  { params }: { params: { location: string } },
+  searchParams: { latitude: string; longitude: string }
+) {
+  return <h1></h1>;
 }
