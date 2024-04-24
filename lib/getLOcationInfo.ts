c5f6aac8e37a4aa2696df506b560988f465e5749
123Somrat <jafaruddin875@gmail.com>
2024-04-24 14:49:55 +0200

    Fetching location name

--- lib/getLOcationInfo.ts
+++ lib/getLOcationInfo.ts
@@ -1,9 +1,36 @@
-export const getLocationInfoData = async (lat:string,lon:string)=>{
-    try{
-       const response = await fetch(`https://api-bdc.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lon}`)
-       const location = await response.json()
-       return location
-    }catch(error){
-        console.log(error)
-    }
-}
+// Fetching location
+export const getLocationInfoData = async (lat: string, lon: string) => {
+  try {
+    const response = await fetch(
+      `https://api-bdc.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lon}`
+    );
+    const location = await response.json();
+    return location;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Fetching location list
+export const getLocationLanList = async () => {
+  try {
+    const response = await fetch("http://localhost:3000/api/location");
+    const location = await response.json();
+    return location;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Fetching single location
+export const getLocationName = async (locationName: string) => {
+  try {
+    const response = await fetch(
+      `http://localhost:3000/api/location/${locationName}`
+    );
+    const locationNam = await response.json();
+    return locationNam;
+  } catch (error) {
+    console.log(error);
+  }
+};
