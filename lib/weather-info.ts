8dbc8d4ff06793f182aba5e79b2b1a566946b40d
123Somrat <jafaruddin875@gmail.com>
2024-04-24 14:14:11 +0200

    Fetching temparatureData and return it

--- lib/weather-info.ts
+++ lib/weather-info.ts
@@ -9,3 +9,18 @@ export const getWeatherData = async (lat: string, lon: string) => {
     console.error(error);
   }
 };
+
+export const getTemparatureData = async (lat: string, lon: string) => {
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.OPEN_WEATHER_MAP_API_KEY}&units=metric`
+    );
+    const temparatureData = await response.json();
+    return temparatureData?.main;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+
+
