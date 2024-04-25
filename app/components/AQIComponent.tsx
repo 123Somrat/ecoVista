685757e38aed0771e452e436e670b37e61361c4d
123Somrat <jafaruddin875@gmail.com>
2024-04-25 13:16:04 +0200

    Fixed card component props type related error

--- app/components/AQIComponent.tsx
+++ app/components/AQIComponent.tsx
@@ -7,7 +7,115 @@ type propTypes = {
 };
 
 export default async function AQIComponent({ lat, lon }: propTypes) {
-  const aqiData = await getAirQualityData(lat, lon);
+  const { main, components } = await getAirQualityData(lat, lon);
 
-  return <Card>{aqiData}</Card>;
+  return (
+  
+    <Card>
+
+      <h6 className="feature-name">Air Pollution & Quality</h6>
+
+      <div className="mt-3 space-y-2 lg:space-y-3">
+        <div className="flex items-center justify-between gap-4">
+          <div className="flex items-center gap-2 text-sm text-[#CADEE8] lg:text-base">
+            <img
+              className="max-w-[18px]"
+              src="./assets/icons/icon_air_element.png"
+              alt="icon"
+            />
+            Air Quality Index
+          </div>
+          <span className="text-right text-sm text-white lg:text-base">
+            Good
+          </span>
+        </div>
+
+        <div className="flex items-center justify-between gap-4">
+          <div className="flex items-center gap-2 text-sm text-[#CADEE8] lg:text-base">
+            <img
+              className="max-w-[18px]"
+              src="./assets/icons/icon_air_element.png"
+              alt="icon"
+            />
+            Carbon Monoxide
+          </div>
+          <span className="text-right text-sm text-white lg:text-base">
+            201.94 µg/m³
+          </span>
+        </div>
+
+        <div className="flex items-center justify-between gap-4">
+          <div className="flex items-center gap-2 text-sm text-[#CADEE8] lg:text-base">
+            <img
+              className="max-w-[18px]"
+              src="./assets/icons/icon_air_element.png"
+              alt="icon"
+            />
+            Nitric Oxide
+          </div>
+          <span className="text-right text-sm text-white lg:text-base">
+            0.01877 ppm
+          </span>
+        </div>
+
+        <div className="flex items-center justify-between gap-4">
+          <div className="flex items-center gap-2 text-sm text-[#CADEE8] lg:text-base">
+            <img
+              className="max-w-[18px]"
+              src="./assets/icons/icon_air_element.png"
+              alt="icon"
+            />
+            Nitrogen Dioxide
+          </div>
+          <span className="text-right text-sm text-white lg:text-base">
+            0.7711 ppm
+          </span>
+        </div>
+
+        <div className="flex items-center justify-between gap-4">
+          <div className="flex items-center gap-2 text-sm text-[#CADEE8] lg:text-base">
+            <img
+              className="max-w-[18px]"
+              src="./assets/icons/icon_air_element.png"
+              alt="icon"
+            />
+            Ozone
+          </div>
+          <span className="text-right text-sm text-white lg:text-base">
+            68.664 µg/m³
+          </span>
+        </div>
+
+        <div className="flex items-center justify-between gap-4">
+          <div className="flex items-center gap-2 text-sm text-[#CADEE8] lg:text-base">
+            <img
+              className="max-w-[18px]"
+              src="./assets/icons/icon_air_element.png"
+              alt="icon"
+            />
+            Sulfur Dioxide
+          </div>
+          <span className="text-right text-sm text-white lg:text-base">
+            0.6407 ppm
+          </span>
+        </div>
+
+        <div className="flex items-center justify-between gap-4">
+          <div className="flex items-center gap-2 text-sm text-[#CADEE8] lg:text-base">
+            <img
+              className="max-w-[18px]"
+              src="./assets/icons/icon_air_element.png"
+              alt="icon"
+            />
+            PM2.5
+          </div>
+          <span className="text-right text-sm text-white lg:text-base">
+            0.5 µg/m³
+          </span>
+        </div>
+      </div>
+    
+    </Card>
+    
+  );
 }
--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,36 +1,18 @@
-interface AirQualityData {
-  main: {
-      aqi: number;
-  };
-  components: {
-      co: number;
-      no: number;
-      no2: number;
-      o3: number;
-      so2: number;
-      pm2_5: number;
-      pm10: number;
-      nh3: number;
-  };
-  dt: number;
+import { ReactElement} from "react";
+interface Props {
+  children: ReactElement | ReactElement[];
+}
+export default function Card({ children }:Props): React.ReactElement {
+  return (
+    <div className="col-span-12 lg:col-span-4 2xl:col-span-6">
+      <div className="card">{children}</div>
+    </div>
+  );
 }
 
-export default function Card({children}:{children:AirQualityData}) {
-  console.log(children)
-  return (
-    <div className="col-span-12 lg:col-span-4 2xl:col-span-3">
-    <div className="card">
-      <h6 className="feature-name">Current Weather</h6>
-      <div className="feature-main">
-        <img
-          className="max-w-20"
-          src="./assets/icons/icon_rain.png"
-          alt="rain icon"
-        />
-        <h3 className="feature-title">Rain</h3>
-        <span className="feature-name">Moderate Rain</span>
-      </div>
-    </div>
-  </div>
-  )
-}
+
+
+
+
+
+
