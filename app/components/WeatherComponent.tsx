7d4796547c92d61bee13a9d90691c5677fcdf36a
123Somrat <jafaruddin875@gmail.com>
2024-04-26 13:07:42 +0200

    Get weather data and pass jsx element as card component child

--- app/components/WeatherComponent.tsx
+++ app/components/WeatherComponent.tsx
@@ -1,9 +1,25 @@
 import { propTypes } from "@/lib/types";
+import { getWeatherData } from "@/lib/weather-info";
+import Image from "next/image";
+import Card from "./Card";
 
+export default async function WeatherComponent({ lat, lon }: propTypes) {
+  const { main, description } = await getWeatherData(lat, lon);
 
-export default function WeatherComponent({lat,lon}:propTypes) {
-    
   return (
-    <div>WeatherComponent</div>
-  )
+    <Card>
+      <h6 className="feature-name">Current Weather</h6>
+      <div className="feature-main">
+        <Image
+          className="max-w-20"
+          src="/icon_rain.png"
+          alt="rain icon"
+          width={18}
+          height={18}
+        />
+        <h3 className="feature-title">{main}</h3>
+        <span className="feature-name">{description}</span>
+      </div>
+    </Card>
+  );
 }
