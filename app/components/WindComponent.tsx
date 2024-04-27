817c4af9d56da92a5be61c9d77bb3ea9265890d6
123Somrat <jafaruddin875@gmail.com>
2024-04-27 12:45:35 +0200

    Passing jsx as a child Card component

--- app/components/WindComponent.tsx
+++ app/components/WindComponent.tsx
@@ -1,9 +1,25 @@
 import { propTypes } from "@/lib/types";
+import { getWindData } from "@/lib/weather-info";
+import Image from "next/image";
+import Card from "./Card";
 
+export default async function WindComponent({ lat, lon }: propTypes) {
+  const { speed, deg, gust } = await getWindData(lat, lon);
 
-export default function WindComponent({lat,lon}:propTypes) {
-    
   return (
-    <div>WindComponent</div>
-  )
+    <Card>
+      <h6 className="feature-name">Wind</h6>
+      <div className="feature-main">
+        <Image
+          className="max-w-20"
+          src="/icon_wind.png"
+          alt="rain icon"
+          width={18}
+          height={18}
+        />
+        <h3 className="feature-title">{speed} meter/sec</h3>
+        <span className="feature-name">{deg} degress</span>
+      </div>
+    </Card>
+  );
 }
