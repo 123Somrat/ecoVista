e9177bc201f73ab5cf6b54008dd2bcb5c85e09ff
123Somrat <jafaruddin875@gmail.com>
2024-05-02 22:16:35 +0200

    destructure contitant countryname and city from response

--- app/components/LocationInfo.tsx
+++ app/components/LocationInfo.tsx
@@ -2,7 +2,7 @@ import { getLocationInfoData } from "@/lib/getLOcationInfo";
 import { propTypes } from "@/lib/types";
 
 export default async function LocationInfo({lat,lon}:propTypes) {
-   const data = await getLocationInfoData(lat,lon)
+   const { continent , countryName , city} = await getLocationInfoData(lat,lon)
    
  
   return <div>LocationInfo</div>;
