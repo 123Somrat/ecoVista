3221bfad316120d55aa94af6e81a7f1d2f9f5bba
123Somrat <jafaruddin875@gmail.com>
2024-05-02 22:13:31 +0200

    Fetching current location data

--- app/components/LocationInfo.tsx
+++ app/components/LocationInfo.tsx
@@ -1,4 +1,9 @@
-export default function LocationInfo({lat,lon}) {
+import { getLocationInfoData } from "@/lib/getLOcationInfo";
+import { propTypes } from "@/lib/types";
 
+export default async function LocationInfo({lat,lon}:propTypes) {
+   const data = await getLocationInfoData(lat,lon)
+   
+ 
   return <div>LocationInfo</div>;
 }
