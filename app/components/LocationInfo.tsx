b90136f170e967bed24d936d471798ba66fecec9
123Somrat <jafaruddin875@gmail.com>
2024-05-02 22:31:53 +0200

    Passing jsx as a children

--- app/components/LocationInfo.tsx
+++ app/components/LocationInfo.tsx
@@ -1,9 +1,26 @@
 import { getLocationInfoData } from "@/lib/getLOcationInfo";
 import { propTypes } from "@/lib/types";
+import Card from "./Card";
+import Image from "next/image";
+export default async function LocationInfo({ lat, lon }: propTypes) {
+  const { continent, countryName, city } = await getLocationInfoData(lat, lon);
 
-export default async function LocationInfo({lat,lon}:propTypes) {
-   const { continent , countryName , city} = await getLocationInfoData(lat,lon)
-   
- 
-  return <div>LocationInfo</div>;
+  return (
+    <Card>
+      <div>
+        <div className="mb-2 flex items-center gap-2">
+          <h2 className="text-3xl font-bold text-white lg:text-4xl 2xl:text-[40px]">
+            {continent}
+          </h2>
+        </div>
+        <p className="text-lg text-[#C4C4C4] lg:text-xl">
+          {countryName} | {city}
+        </p>
+        <div className="flex items-center gap-2 text-xs text-[#92B6F5] lg:text-sm">
+          <span>{new Date().toLocaleTimeString()}</span>{" "}
+          <span>{new Date().toLocaleDateString()}</span>
+        </div>
+      </div>
+    </Card>
+  );
 }
